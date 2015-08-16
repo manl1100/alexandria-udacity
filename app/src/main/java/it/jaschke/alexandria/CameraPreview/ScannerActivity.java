70997d279ae9d0517022ba0b8b03f1ee94f3f45a
Manuel Sanchez <manuelsanchez1100@gmail.com>
2015-08-16 14:10:04 -0500

    Fixed debugging issue,  scanning functionality completed (without install of separate app)

--- app/src/main/java/it/jaschke/alexandria/CameraPreview/ScannerActivity.java
+++ app/src/main/java/it/jaschke/alexandria/CameraPreview/ScannerActivity.java
@@ -1,11 +1,9 @@
 package it.jaschke.alexandria.CameraPreview;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.zxing.Result;
 
@@ -29,30 +27,24 @@ public class ScannerActivity extends Activity implements ZXingScannerView.Result
     @Override
     public void onResume() {
         super.onResume();
-        mZXingScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
-        mZXingScannerView.startCamera();          // Start camera on resume
+        mZXingScannerView.setResultHandler(this);
+        mZXingScannerView.startCamera();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mZXingScannerView.stopCamera();           // Stop camera on pause
+        mZXingScannerView.stopCamera();
     }
 
     @Override
     public void handleResult(Result result) {
-        // Do something with the result here
-        Context context = getApplicationContext();
-        CharSequence text = result.getText();
-        int duration = Toast.LENGTH_SHORT;
+        Log.v(TAG, result.getText());
+        Log.v(TAG, result.getBarcodeFormat().toString());
 
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
         Intent intent = new Intent();
         intent.putExtra("EAN", result.getText());
-        setResult(1001, intent);
+        setResult(REQUEST_CODE, intent);
         finish();
-//        Log.v(TAG, result.getText()); // Prints scan results
-//        Log.v(TAG, result.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
     }
 }
